dec0b36665c4bd64962720fec225e83b298add86
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-01 19:11:38 +0200

    add classes : 'text-white bg-dark p-5 text-center'

--- myscript.js
+++ myscript.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
             createApp ({
                 data(){
                     return{
-                        message: 'Helllo Vue!'
+                        message: 'Hello Vue!',
+                        classes : 'text-white bg-dark p-5 text-center'
                     }
                 }
             }).mount ('#app')
