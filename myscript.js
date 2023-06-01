d9ddd93c8db51fd9e86d1d19ea1c8b4be53d9999
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-01 21:41:07 +0200

    centered img and add mt-5

--- myscript.js
+++ myscript.js
@@ -5,7 +5,9 @@ const { createApp } = Vue
                 data(){
                     return{
                         message: 'Hello Vue!',
-                        classes : 'text-white bg-dark p-5 text-center'
+                        classes : 'text-white bg-dark p-5 text-center',
+                        vueimg : 'd-flex justify-content-center',
+                        vueimgcolor : 'mt-5'
                     }
                 }
             }).mount ('#app')
