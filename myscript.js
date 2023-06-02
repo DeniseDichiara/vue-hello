5d536cda28c99238cc519b68060f2b1ab7e7769d
Denise-Dichiara <dichiara.d@gmail.com>
2023-06-02 16:29:45 +0200

    added methods

--- myscript.js
+++ myscript.js
@@ -6,8 +6,17 @@ const { createApp } = Vue
                     return{
                         message: 'Hello Vue!',
                         classes : 'text-white bg-dark p-5 text-center',
+                        buttonstart : 'ms-3',
                         vueimg : 'd-flex justify-content-center',
-                        vueimgcolor : 'mt-5'
+                        vueimgcolor : 'mt-5',
+                        
                     }
-                }
+                },
+
+                methods:{
+                    funzione(){
+                        this.message = 'Discover Vue'
+                    }
+                },
+
             }).mount ('#app')
